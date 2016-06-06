61bd6a16d83bf89a1e43083c4ddae401cdf7278d
jaracil <pepe@diselpro.com>
2016-06-06 11:06:42 +0200

    Simplfy SmartWriter.

--- smartio.go
+++ smartio.go
@@ -53,22 +53,21 @@ func (sr *SmartReader) GetLast() int64 {
 }
 
 func (sr *SmartReader) Read(p []byte) (n int, err error) {
+	if atomic.LoadUint32(&sr.limited) != 0 && atomic.LoadInt64(&sr.limit) < 0 {
+		err = io.EOF
+		return
+	}
 	n, err = sr.r.Read(p)
 	atomic.AddInt64(&sr.limit, int64(-n))
 	atomic.AddInt64(&sr.total, int64(n))
 	atomic.StoreInt64(&sr.last, time.Now().Unix())
-	if atomic.LoadUint32(&sr.limited) != 0 && atomic.LoadInt64(&sr.limit) < 0 {
-		err = io.EOF
-	}
 	return
 }
 
 type SmartWriter struct {
-	w       io.Writer
-	total   int64
-	limit   int64
-	last    int64
-	limited uint32 //atomic bool
+	w     io.Writer
+	total int64
+	last  int64
 }
 
 func NewSmartWriter(writer io.Writer) *SmartWriter {
@@ -83,20 +82,6 @@ func (sw *SmartWriter) GetTotal() int64 {
 	return atomic.LoadInt64(&sw.total)
 }
 
-func (sw *SmartWriter) SetLimit(limit int64) int64 {
-	last := atomic.SwapInt64(&sw.limit, limit)
-	if limit != 0 {
-		atomic.StoreUint32(&sw.limited, 1)
-	} else {
-		atomic.StoreUint32(&sw.limited, 0)
-	}
-	return last
-}
-
-func (sw *SmartWriter) GetLimit() int64 {
-	return atomic.LoadInt64(&sw.limit)
-}
-
 func (sw *SmartWriter) SetLast(last int64) int64 {
 	return atomic.SwapInt64(&sw.last, last)
 }
@@ -106,30 +91,8 @@ func (sw *SmartWriter) GetLast() int64 {
 }
 
 func (sw *SmartWriter) Write(p []byte) (n int, err error) {
-	if atomic.LoadUint32(&sw.limited) != 0 && atomic.LoadInt64(&sw.limit) < int64(len(p)) {
-		return 0, io.EOF
-	}
-	var chunkSize int
-	for {
-		sz := len(p)
-		if sz == 0 {
-			break
-		}
-		if sz > defaultChunkSize {
-			sz = defaultChunkSize
-		}
-		chunkSize, err = sw.w.Write(p[0:sz])
-		n += chunkSize
-		p = p[chunkSize:]
-		atomic.AddInt64(&sw.limit, int64(-chunkSize))
-		atomic.AddInt64(&sw.total, int64(chunkSize))
-		atomic.StoreInt64(&sw.last, time.Now().Unix())
-		if err != nil {
-			break
-		}
-		if atomic.LoadUint32(&sw.limited) != 0 && atomic.LoadInt64(&sw.limit) < 0 {
-			return n, io.EOF
-		}
-	}
+	n, err = sw.w.Write(p)
+	atomic.AddInt64(&sw.total, int64(n))
+	atomic.StoreInt64(&sw.last, time.Now().Unix())
 	return
 }
